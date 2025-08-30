6687526f2615da9ccf2d1cc359df03839a47de05
Jhon Quintero <jfqm7710@gmail.com>
2025-08-29 21:36:56 -0500

    first commit

---
+++ src/common/exceptions/rpc-custom-exception.filter.ts
@@ -0,0 +1,35 @@
+import { Catch, ArgumentsHost, ExceptionFilter } from '@nestjs/common';
+import { RpcException } from '@nestjs/microservices';
+import { Response } from 'express';
+
+function isRpcError(
+  error: unknown,
+): error is { status: number; message: string } {
+  return (
+    typeof error === 'object' &&
+    error !== null &&
+    'status' in error &&
+    'message' in error
+  );
+}
+
+@Catch(RpcException)
+export class RpcCustomExceptionFilter implements ExceptionFilter {
+  catch(exception: RpcException, host: ArgumentsHost) {
+    const ctx = host.switchToHttp();
+    const response = ctx.getResponse<Response>();
+
+    const rpcError = exception.getError();
+
+    if (isRpcError(rpcError)) {
+      const status = isNaN(+rpcError.status) ? 400 : Number(rpcError.status);
+      return response.status(status).json(rpcError);
+    }
+
+    return response.status(400).json({
+      status: 400,
+      message:
+        typeof rpcError === 'string' ? rpcError : JSON.stringify(rpcError),
+    });
+  }
+}
